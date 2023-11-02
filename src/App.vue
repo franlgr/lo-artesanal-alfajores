deca133b9b7845680a8f98533dffcde329844845
franlgr <grilfran90@gmail.com>
2023-11-02 00:40:19 -0300

    anality

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,12 @@
     <router-view />
   </div>
 </template>
+     <!-- Google tag (gtag.js) -->
+     <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-ND616K92FZ"></script> -->
+     <script setup>
+       window.dataLayer = window.dataLayer || [];
+       function gtag(){dataLayer.push(arguments);}
+       gtag('js', new Date());
+ 
+       gtag('config', 'G-ND616K92FZ');
+     </script>
