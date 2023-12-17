a84231d612418913499fdadccbf453822b36b21f
franlgr <grilfran90@gmail.com>
2023-12-17 20:37:04 -0300

    asdaskjd

--- src/components/GalleryImg.vue
+++ src/components/GalleryImg.vue
@@ -2,51 +2,33 @@
 
 
 
-<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf1.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf2.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf3.jpeg" alt="">
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf4.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf5.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf6.jpeg" alt="">
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf7.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf9.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf8.jpeg" alt="">
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf5.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf7.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="../assets/img/alf4.jpeg" alt="">
-        </div>
-    </div>
-</div>
+<div class="flex space-x-4 justify-center">
+    <figure class="relative max-w-sm  cursor-pointer ">
+      <a href="#">
+        <img class="rounded-lg" src="https://res.cloudinary.com/doznjtpmk/image/upload/v1702855115/admin-web/maifam0p5gfkuntmj6n9.jpg" alt="image description">
+      </a>
+      <figcaption class="absolute px-4 text-lg text-white bottom-6">
+        <!-- <p>Do you want to get notified when a new component is added to Flowbite?</p> -->
+      </figcaption>
+    </figure>
+
+    <figure class="relative max-w-sm  cursor-pointer">
+      <a href="#">
+        <img class="rounded-lg" src="https://res.cloudinary.com/doznjtpmk/image/upload/v1702856134/admin-web/t1zekzcywvczxbmkf00f.jpg" alt="image description">
+      </a>
+      <figcaption class="absolute px-4 text-lg text-white bottom-6">
+        <!-- <p>Do you want to get notified when a new component is added to Flowbite?</p> -->
+      </figcaption>
+    </figure>
+
+    <figure class="relative max-w-sm  cursor-pointer">
+      <a href="#">
+        <img class="rounded-lg" src="https://res.cloudinary.com/doznjtpmk/image/upload/v1702855861/admin-web/uzrqzmtyb46qeb6rrrw4.jpg" alt="image description">
+      </a>
+      <figcaption class="absolute px-4 text-lg text-white bottom-6">
+        <!-- <p>Do you want to get notified when a new component is added to Flowbite?</p> -->
+      </figcaption>
+    </figure>
+  </div>
 
 </template>
